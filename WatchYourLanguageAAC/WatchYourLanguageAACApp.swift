9960122ec5fd9254946f8e7bf556578b12dc3a0a
Humphrey Curtis <humphreywcurtis@gmail.com>
2026-07-06 15:51:30 -0700

    Initial Commit

---
+++ WatchYourLanguageAAC/WatchYourLanguageAACApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchYourLanguageAACApp.swift
+//  WatchYourLanguageAAC
+//
+//  Created by Humphrey Curtis on 06/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct WatchYourLanguageAACApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
